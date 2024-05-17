e29d622e8e289ba3b234a6c6c7b2033a3e9df5e1
williamw04 <williamwu120@gmail.com>
2024-05-17 13:40:18 -0400

    finished agent implementation

--- agent.py
+++ agent.py
@@ -15,6 +15,8 @@ class Agent:
     self.epsilon = 0 # randomness
     self.gamma = 0 # discount rate
     self.memory = deque(maxlen=MAX_MEMORY) #pop left when full
+    self.model = None # TODO
+    self.trainer = None # TODO
     # TODO: model, trainer
 
   def get_state(self, game):
@@ -66,16 +68,35 @@ class Agent:
 
 
   def remember(self, state, action, reward, next_state, done):
-    pass
+    self.memory.append(state, action, reward, next_state, done)
 
   def train_long_memory(self):
-    pass
+    if len(self.memory) > BATCH_SIZE:
+      mini_sample = random.sample(self.memory, BATCH_SIZE)
+    else:
+      mini_Sample = self.memory
+
+    states, actions, rewards, next_states, dones = zip(*mini_sample)
+    self.trainer.train_step(states, actions, rewards, next_states, dones)
 
   def train_short_memory(self, state, action, reward, next_state, done):
-    pass
+    self.trainer.train_step(state, action, reward, next_state, done)
 
   def get_action(self, state):
-    pass
+    # random moves: tradeoff exploration / exploitation
+
+    self.epsilson = 80 - self.n_games
+    final_move = [0,0,0]
+    if random.randint(0,200) < self.epsilon:
+      move = random.randint(0, 2)
+      final_move[move] = 1
+    else:
+      state0 = torch.tensor(state, dtype=torch.float)
+      prediction = self.model.predict(state0)
+      move = torch.argmax(prediction).item()
+      final_move[move] = 1
+
+    return final_move
 
 def train():
   plot_scores = []
